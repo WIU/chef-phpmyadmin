19f98500c860db2d419cf537c784194a43dcb5a0
Matt Mencel <matt@techminer.net>
2017-12-07 17:13:04 -0600

    Switch to php_fpm_pool

--- recipes/default.rb
+++ recipes/default.rb
@@ -122,21 +122,15 @@ template "#{home}/config.inc.php" do
 end
 
 if (node['phpmyadmin'].attribute?('fpm') && node['phpmyadmin']['fpm'])
- 	php_fpm 'phpmyadmin' do
-	  action :add
+ 	php_fpm_pool 'phpmyadmin' do
+	  action :install
 	  user user
 	  group group
-	  socket true
-	  socket_path node['phpmyadmin']['socket']
-	  socket_user user
-	  socket_group group
-	  socket_perms '0666'
 	  start_servers 2
 	  min_spare_servers 2
 	  max_spare_servers 8
 	  max_children 8
-	  terminate_timeout (node['php']['ini_settings']['max_execution_time'].to_i + 20)
-	  value_overrides({
+	  additional_config({
 	    :error_log => "#{node['php']['fpm_log_dir']}/phpmyadmin.log"
 	  })
 	end
